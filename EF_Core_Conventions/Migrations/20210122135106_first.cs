d81ee000c6a2d96f7c0bcd0c8e99728936eb61c8
Wamik Yasin <wamik.yasin@techverx.com>
2021-01-26 15:31:29 +0500

    first commit

---
+++ EF_Core_Conventions/Migrations/20210122135106_first.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EF_Core_Conventions.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    GradeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GradeName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.GradeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentAge = table.Column<int>(type: "int", nullable: false),
+                    GradeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Grades_GradeId",
+                        column: x => x.GradeId,
+                        principalTable: "Grades",
+                        principalColumn: "GradeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GradeId",
+                table: "Students",
+                column: "GradeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+        }
+    }
+}
---
+++ EF_Core_Conventions/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core_Conventions
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+
+        public int GradeId { get; set; }
+        public Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public partial class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=SchoolDB1;Trusted_Connection=True;");
+        }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasOne<Grade>(s => s.Grade)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.GradeId);
+        }*/
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var gtd = new Grade()
+                {
+                    GradeName = "A"
+                };
+
+                var gtd1 = new Grade()
+                {
+                    GradeName = "B"
+                };
+
+                var gtd2 = new Grade()
+                {
+                    GradeName = "C"
+                };
+
+                var gtd3 = new Grade()
+                {
+                    GradeName = "D"
+                };
+
+                var std = new Student()
+                {
+                    StudentName = "Wamik",
+                    StudentAge = 23,
+                    GradeId = 1
+                };
+
+                var std1 = new Student()
+                {
+                    StudentName = "Ihtisham",
+                    StudentAge = 20,
+                    GradeId = 2
+                };
+
+                var std2 = new Student()
+                {
+                    StudentName = "afaq",
+                    StudentAge = 29,
+                    GradeId = 2
+                };
+
+                var std3 = new Student()
+                {
+                    StudentName = "Askari",
+                    StudentAge = 25,
+                    GradeId = 2
+                };
+
+                var std4 = new Student()
+                {
+                    StudentName = "Umair",
+                    StudentAge = 21,
+                    GradeId = 3
+                };
+
+                /*context.Grades.Add(gtd);
+                context.Grades.Add(gtd1);
+                context.Grades.Add(gtd2);
+                context.Grades.Add(gtd3);
+
+                context.Students.Add(std);
+                context.Students.Add(std1);
+                context.Students.Add(std2);
+                context.Students.Add(std3);
+                context.Students.Add(std4);*/
+
+                context.SaveChanges();
+
+                //RAW SQL query
+                var students = context.Students.FromSqlRaw("Select * from Students where StudentName = 'wamik'");
+
+                foreach (var s in students)
+                {
+                    Console.WriteLine(s.StudentName);
+                    Console.WriteLine(s.StudentAge);
+                    Console.WriteLine(s.GradeId);
+                    Console.WriteLine(s.GradeId);
+                }
+
+                //Eager Loading
+                Console.WriteLine("----------------------------Eager Loading----------------------------");
+
+                var stud1 = context.Grades.Include("Students");
+
+                foreach (var s in stud1)
+                {
+                    foreach (var a in s.Students)
+                    {
+                        Console.WriteLine(s.GradeName + " " + a.StudentName);
+                    }
+                }
+            }
+        }
+        
+    }
+}
